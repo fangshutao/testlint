2c698fe77efdfec13e852291c11ec8608da08e70
fangst <fangst722@gmail.com>
2021-04-26 11:21:50 +0800

    测试校验

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "src/**/*.{css,sass,less}": "stylelint --fix",
   "src/**/*.{js,jsx,ts,tsx}": "eslint --ext .js,.jsx,.ts,.tsx",
   "src/**/*.{js,jsx,tsx,ts,less,md,json}": [
